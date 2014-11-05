f7cd7142a339bcd47d9402f6ac56c7d71c057ea7
Will Hohyon Ryu <hohyonryu@twitter.com>
2014-11-04 16:37:48 -0800

    updated dictionary

--- src/main/scala/com/twitter/penguin/korean/qa/BatchTokenizeTweets.scala
+++ src/main/scala/com/twitter/penguin/korean/qa/BatchTokenizeTweets.scala
@@ -51,12 +51,7 @@ object BatchTokenizeTweets {
         val t1 = System.currentTimeMillis()
 
         if (VERBOSE) {
-          if (parsed.length > 2
-            && parsed.forall(_.pos == KoreanPos.Noun)
-            && parsed.forall(_.unknown == false)
-            && parsed.forall(_.text.length > 1)) {
-            println(line.trim + "\t" + parsed.map(_.text).mkString(" "))
-          }
+          println(parsed.map(t => t.text + "/" + t.pos).mkString(" "))
         }
         ParseTime(t1 - t0, line.trim) :: l
       case (l: List[ParseTime], line: String) => l
