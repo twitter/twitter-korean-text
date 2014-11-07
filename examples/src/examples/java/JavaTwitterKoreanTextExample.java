1718ec4388e94bff6fa6c8f6f5fe5cb12060275e
Will Hohyon Ryu <hohyonryu@twitter.com>
2014-11-07 09:18:26 -0800

    updated examples

---
+++ examples/src/examples/java/JavaTwitterKoreanTextExample.java
@@ -0,0 +1,56 @@
+/*
+ * Twitter Korean Text - Scala library to process Korean text
+ *
+ * Copyright 2014 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package examples.java;
+
+import java.util.List;
+
+import com.twitter.penguin.korean.TwitterKoreanProcessorJava;
+import com.twitter.penguin.korean.tokenizer.KoreanTokenizer;
+
+public class JavaTwitterKoreanTextExample {
+  public static void main(String[] args) {
+    // Tokenize with normalization + stemmer
+    TwitterKoreanProcessorJava processor = new TwitterKoreanProcessorJava.Builder().build();
+
+    List<String> parsedStrings = processor.tokenizeToStrings("한국어를 처리하는 예시입니닼ㅋㅋㅋㅋㅋ");
+    System.out.println(parsedStrings);
+    // output: [한국어, 를, 처리, 하다, 예시, 이다, ㅋㅋ]
+
+    List<KoreanTokenizer.KoreanToken> parsed = processor.tokenize("한국어를 처리하는 예시입니닼ㅋㅋㅋㅋㅋ");
+    System.out.println(parsed);
+    // output: [한국어Noun, 를Josa, 처리Noun, 하다Verb, 예시Noun, 이다Adjective, ㅋㅋKoreanParticle]
+
+
+    // Tokenize with neither normalization nor stemmer
+    processor = new TwitterKoreanProcessorJava.Builder()
+        .disableNormalizer()
+        .disableStemmer()
+        .build();
+
+    parsedStrings = processor.tokenizeToStrings("한국어를 처리하는 예시입니닼ㅋㅋㅋㅋㅋ");
+    System.out.println(parsedStrings);
+    // output: [한국어, 를, 처리, 하는, 예시, 입, 니, 닼, ㅋㅋㅋㅋㅋ]
+
+    parsed = processor.tokenize("한국어를 처리하는 예시입니닼ㅋㅋㅋㅋㅋ");
+    System.out.println(parsed);
+    // output: [한국어Noun, 를Josa, 처리Noun, 하는Verb, 예시Noun, 입Noun, 니Josa, 닼Noun*, ㅋㅋㅋㅋㅋKoreanParticle]
+
+
+  }
+}
---
+++ examples/src/examples/scala/ScalaTwitterKoreanTextExample.scala
@@ -0,0 +1,22 @@
+import com.twitter.penguin.korean.TwitterKoreanProcessor
+import com.twitter.penguin.korean.tokenizer.KoreanTokenizer
+
+object ScalaTwitterKoreanTextExample {
+  def main(args: Array[String]) {
+    // Tokenize into List<String>
+    val parsed: Seq[String] = TwitterKoreanProcessor.tokenizeToStrings("한국어를 처리하는 예시입니닼ㅋㅋㅋㅋㅋ")
+    println(parsed)
+    // ArraySeq(한국어, 를, 처리, 하다, 예시, 이다, ㅋㅋ)
+
+    // Tokenize with Part-of-Speech information
+    val parsedPos: Seq[KoreanTokenizer.KoreanToken] = TwitterKoreanProcessor.tokenize("한국어를 처리하는 예시입니닼ㅋㅋㅋㅋㅋ")
+    println(parsedPos)
+    // ArraySeq(한국어Noun, 를Josa, 처리Noun, 하다Verb, 예시Noun, 이다Adjective, ㅋㅋKoreanParticle)
+
+    // Tokenize without normalization and stemming
+    val parsedPosParsingOnly: Seq[KoreanTokenizer.KoreanToken] = TwitterKoreanProcessor
+      .tokenize("한국어를 처리하는 예시입니닼ㅋㅋㅋㅋㅋ", normalizization = false, stemming = false)
+    println(parsedPosParsingOnly)
+    // ArraySeq(한국어Noun, 를Josa, 처리Noun, 하는Verb, 예시Noun, 입Noun, 니Josa, 닼Noun*, ㅋㅋㅋㅋㅋKoreanParticle)
+  }
+}
