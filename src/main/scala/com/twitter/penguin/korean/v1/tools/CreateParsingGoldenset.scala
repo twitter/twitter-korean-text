fcc85cba0ac32d0e1ff567c2d2e3239302e1d1db
Will Hohyon Ryu <hohyonryu@twitter.com>
2014-11-26 21:08:59 -0800

    v1 merged

---
+++ src/main/scala/com/twitter/penguin/korean/v1/tools/CreateParsingGoldenset.scala
@@ -0,0 +1,73 @@
+/*
+ * Twitter Korean Text - Scala library to process Korean text
+ *
+ * Copyright 2014 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.twitter.penguin.korean.v1.tools
+
+import java.io.FileOutputStream
+import java.util.zip.GZIPOutputStream
+
+import com.twitter.penguin.korean.v1.TwitterKoreanProcessor
+import TwitterKoreanProcessor._
+import com.twitter.penguin.korean.thriftscala._
+import com.twitter.penguin.korean.v1.util.KoreanDictionaryProvider._
+import org.apache.thrift.protocol.TBinaryProtocol
+import org.apache.thrift.transport.TIOStreamTransport
+
+/**
+ * Create Korean Parsing goldenset from the goldenset resource that contains goldenset chunks.
+ * The first argument is a gzipped output file.
+ *
+ * usage: ./pants goal run src/scala/com/twitter/penguin/korean/tools:create_parsing_goldenset
+ * --jvm-run-args="/Users/hohyonryu/workspace/penguin-binaries/tests/com/twitter/penguin/korean/goldenset.txt.gz"
+ */
+object CreateParsingGoldenset {
+  def main(args: Array[String]) {
+    if (args.length != 1) {
+      throw new IllegalArgumentException("Please specify an output file")
+    }
+
+    System.err.println("Reading the goldenset..")
+
+    val parsed = readFileByLineFromResources("goldenset.txt").flatMap {
+      case line if line.length > 0 =>
+        val chunk = line.trim
+        val parsed = tokenizeWithNormalization(chunk)
+        Some(ParseItem(chunk, parsed.map(p => KoreanTokenThrift(p.text, p.pos.id, p.unknown))))
+      case line => None
+    }.toSeq
+
+
+    val outputFile: String = args(0)
+
+    System.err.println("Writing the new goldenset to " + outputFile)
+
+    val out = new GZIPOutputStream(new FileOutputStream(outputFile))
+    val binaryOut = new TBinaryProtocol(new TIOStreamTransport(out))
+    ParsingGoldenset(parsed).write(binaryOut)
+    out.close()
+
+    System.err.println("Testing the new goldenset " + outputFile)
+
+    val input = readGzipTBininaryFromFile(outputFile)
+    val loaded = ParsingGoldenset.decode(input).goldenset
+
+    assert(loaded.equals(parsed))
+
+    System.err.println("Updated goldenset in " + outputFile)
+  }
+}
---
+++ src/test/scala/com/twitter/penguin/korean/v1/tokenizer/KoreanChunkerTest.scala
@@ -0,0 +1,85 @@
+/*
+ * Twitter Korean Text - Scala library to process Korean text
+ *
+ * Copyright 2014 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.twitter.penguin.korean.v1.tokenizer
+
+import com.twitter.penguin.korean.v1.tokenizer.KoreanChunker._
+import com.twitter.penguin.korean.v1.tokenizer.KoreanTokenizer.KoreanToken
+import com.twitter.penguin.korean.v1.util.KoreanPos._
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class KoreanChunkerTest extends FunSuite {
+
+  test("getChunks should correctly split a string into Korean-sensitive chunks") {
+    assert(
+      getChunks("안녕? iphone6안녕? 세상아?")
+          === "안녕 ? iphone 6 안녕 ? 세상아 ?".split(" ").toSeq
+    )
+
+    assert(
+      getChunks("This is an 한국어가 섞인 English tweet.")
+          === "This is an 한국어가 섞인 English tweet .".split(" ").toSeq
+    )
+
+    assert(
+      getChunks("이 日本것은 日本語Eng")
+          === "이 日本 것은 日本語 Eng".split(" ").toSeq
+    )
+
+    assert(
+      getChunks("무효이며")
+          === Seq("무효이며")
+    )
+
+    assert(
+      getChunks("#해쉬태그 이라는 것 #hash @hello 123 이런이런 #여자최애캐_5명으로_취향을_드러내자")
+          === "#해쉬태그 이라는 것 #hash @hello 123 이런이런 #여자최애캐_5명으로_취향을_드러내자".split(" ").toSeq
+    )
+  }
+
+  test("getChunkTokens should correctly find chunks with correct POS tags") {
+    assert(
+      chunk("한국어와 English와 1234와 pic.twitter.com " +
+          "http://news.kukinews.com/article/view.asp?" +
+          "page=1&gCode=soc&arcid=0008599913&code=41121111 " +
+          "hohyonryu@twitter.com 갤럭시 S5").mkString(" ")
+          ===
+          "한국어와Korean EnglishAlpha 와Korean 1234Number 와Korean " +
+              "pic.twitter.comURL " +
+              "http://news.kukinews.com/article/view.asp?" +
+              "page=1&gCode=soc&arcid=0008599913&code=41121111URL " +
+              "hohyonryu@twitter.comEmail 갤럭시Korean SAlpha 5Number"
+    )
+
+    assert(
+      chunk("우와!!! 완전ㅋㅋㅋㅋ")
+          === Seq(
+        KoreanToken("우와", Korean), KoreanToken("!!!", Punctuation),
+        KoreanToken("완전", Korean), KoreanToken("ㅋㅋㅋㅋ", KoreanParticle))
+    )
+
+    assert(
+      chunk("@nlpenguin @edeng #korean_tokenizer_rocks 우하하")
+          === Seq(KoreanToken("@nlpenguin", ScreenName), KoreanToken("@edeng", ScreenName),
+        KoreanToken("#korean_tokenizer_rocks", Hashtag), KoreanToken("우하하", Korean))
+    )
+  }
+}
