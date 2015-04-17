be9be6249d041ab7777255268c5edbf971238a14
Will Hohyon Ryu <hohyonryu@twitter.com>
2015-04-17 11:43:06 -0700

    Added TwitterKoreanTextPos and TwitterKoreanTextToken

---
+++ src/main/java/com/twitter/penguin/korean/TwitterKoreanTextToken.java
@@ -0,0 +1,43 @@
+/*
+ * Twitter Korean Text - Scala library to process Korean text
+ *
+ * Copyright 2015 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.twitter.penguin.korean;
+
+public class TwitterKoreanTextToken {
+  String text;
+  TwitterKoreanTextPos pos;
+  int offset;
+  int length;
+  boolean unknown;
+
+  public TwitterKoreanTextToken(String text, TwitterKoreanTextPos pos, int offset, int length, boolean unknown) {
+    this.text = text;
+    this.pos = pos;
+    this.offset = offset;
+    this.length = length;
+    this.unknown = unknown;
+  }
+
+  @Override
+  public String toString() {
+    String unknownStar = "";
+    if (unknown){
+      unknownStar = "*";
+    }
+    return String.format("%s%s(%s: %d, %d)", text, unknownStar, pos.toString(), offset, length);
+  }
+}
