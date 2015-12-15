771cc811f8ac05298e5c673f74e13e669a32f2f2
Will Hohyon Ryu <hohyonryu@twitter.com>
2015-12-14 23:39:21 -0800

    2015

--- src/test/scala/com/twitter/penguin/korean/tokenizer/KoreanDetokenizerTest.scala
+++ src/test/scala/com/twitter/penguin/korean/tokenizer/KoreanDetokenizerTest.scala
@@ -1,7 +1,7 @@
 /*
  * Twitter Korean Text - Scala library to process Korean text
  *
- * Copyright 2014 Twitter, Inc.
+ * Copyright 2015 Twitter, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
