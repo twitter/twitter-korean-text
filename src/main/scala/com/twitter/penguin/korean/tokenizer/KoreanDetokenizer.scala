afafeae1b55d1a426a06f6c04346455298dbd5ed
Will Hohyon Ryu <hohyonryu@twitter.com>
2015-12-14 23:34:05 -0800

    Added comments

--- src/main/scala/com/twitter/penguin/korean/tokenizer/KoreanDetokenizer.scala
+++ src/main/scala/com/twitter/penguin/korean/tokenizer/KoreanDetokenizer.scala
@@ -1,23 +1,46 @@
+/*
+ * Twitter Korean Text - Scala library to process Korean text
+ *
+ * Copyright 2015 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.twitter.penguin.korean.tokenizer
 
 import com.twitter.penguin.korean.tokenizer.KoreanTokenizer.KoreanToken
 import com.twitter.penguin.korean.util.KoreanPos
 
 /**
-  * Created by hohyonryu on 12/14/15.
+  * Detokenizes a list of tokenized words into a readable sentence.
   */
 object KoreanDetokenizer {
-  val SuffixPos = Set(KoreanPos.Josa, KoreanPos.Eomi, KoreanPos.PreEomi, KoreanPos.Suffix, KoreanPos.Punctuation)
+  val SuffixPos = Set(KoreanPos.Josa, KoreanPos.Eomi, KoreanPos.PreEomi, KoreanPos.Suffix,
+    KoreanPos.Punctuation)
   val PrefixPos = Set(KoreanPos.NounPrefix, KoreanPos.VerbPrefix)
 
   def detokenize(input: Iterable[String]) = {
-    val (spaceGuide, index) = input.foldLeft((Set[Int](), 0)) {
-      case ((output: Set[Int], i: Int), word: String) =>
-        (output + (i + word.length), i + word.length)
-    }
+    // Space guide prevents tokenizing a word that was not tokenized in the input.
+    val spaceGuide: Set[Int] = getSpaceGuide(input)
 
+    // Tokenize a merged text with the space guide.
     val tokenized = KoreanTokenizer.tokenize(input.mkString(""), TokenizerProfile(spaceGuide = spaceGuide))
 
+    // Attach suffixes and prefixes.
+    collapseTokens(tokenized).mkString(" ")
+  }
+
+  private def collapseTokens(tokenized: Seq[KoreanToken]): List[String] = {
     val (output, prefix) = tokenized.foldLeft((List[String](), false)) {
       case ((output: List[String], prefix: Boolean), token: KoreanToken) =>
         if (prefix || SuffixPos.contains(token.pos)) {
@@ -29,7 +52,14 @@ object KoreanDetokenizer {
           (output :+ token.text, false)
         }
     }
+    output
+  }
 
-    output.mkString(" ")
+  private def getSpaceGuide(input: Iterable[String]): Set[Int] = {
+    val (spaceGuide, index) = input.foldLeft((Set[Int](), 0)) {
+      case ((output: Set[Int], i: Int), word: String) =>
+        (output + (i + word.length), i + word.length)
+    }
+    spaceGuide
   }
 }
