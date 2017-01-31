bdcb81b4549f93c819072983e2f322fcc42a4152
Will Hohyon Ryu <will.ryu@airbnb.com>
2017-01-30 23:45:47 -0800

    Fixed Java Example

--- examples/src/main/java/JavaOpenKoreanTextExample.java
+++ examples/src/main/java/JavaOpenKoreanTextExample.java
@@ -20,39 +20,39 @@ import java.util.List;
 
 import scala.collection.Seq;
 
-import org.openkoreantext.processor.TwitterKoreanProcessor;
-import org.openkoreantext.processor.TwitterKoreanProcessorJava;
+import org.openkoreantext.processor.OpenKoreanTextProcessor;
+import org.openkoreantext.processor.OpenKoreanTextProcessorJava;
 import org.openkoreantext.processor.phrase_extractor.KoreanPhraseExtractor;
 import org.openkoreantext.processor.tokenizer.KoreanTokenizer;
 
-public class JavaTwitterKoreanTextExample {
+public class JavaOpenKoreanTextProcessorExample {
   public static void main(String[] args) {
     String text = "한국어를 처리하는 예시입니닼ㅋㅋㅋㅋㅋ #한국어";
 
     // Normalize
-    CharSequence normalized = TwitterKoreanProcessorJava.normalize(text);
+    CharSequence normalized = OpenKoreanTextProcessorJava.normalize(text);
     System.out.println(normalized);
     // 한국어를 처리하는 예시입니다ㅋㅋ #한국어
 
 
     // Tokenize
-    Seq<KoreanTokenizer.KoreanToken> tokens = TwitterKoreanProcessorJava.tokenize(normalized);
-    System.out.println(TwitterKoreanProcessorJava.tokensToJavaStringList(tokens));
+    Seq<KoreanTokenizer.KoreanToken> tokens = OpenKoreanTextProcessorJava.tokenize(normalized);
+    System.out.println(OpenKoreanTextProcessorJava.tokensToJavaStringList(tokens));
     // [한국어, 를, 처리, 하는, 예시, 입니, 다, ㅋㅋ, #한국어]
-    System.out.println(TwitterKoreanProcessorJava.tokensToJavaKoreanTokenList(tokens));
+    System.out.println(OpenKoreanTextProcessorJava.tokensToJavaKoreanTokenList(tokens));
     // [한국어(Noun: 0, 3), 를(Josa: 3, 1),  (Space: 4, 1), 처리(Noun: 5, 2), 하는(Verb: 7, 2),  (Space: 9, 1), 예시(Noun: 10, 2), 입니(Adjective: 12, 2), 다(Eomi: 14, 1), ㅋㅋ(KoreanParticle: 15, 2),  (Space: 17, 1), #한국어(Hashtag: 18, 4)]
 
 
     // Stemming
-    Seq<KoreanTokenizer.KoreanToken> stemmed = TwitterKoreanProcessorJava.stem(tokens);
-    System.out.println(TwitterKoreanProcessorJava.tokensToJavaStringList(stemmed));
+    Seq<KoreanTokenizer.KoreanToken> stemmed = OpenKoreanTextProcessorJava.stem(tokens);
+    System.out.println(OpenKoreanTextProcessorJava.tokensToJavaStringList(stemmed));
     // [한국어, 를, 처리, 하다, 예시, 이다, ㅋㅋ, #한국어]
-    System.out.println(TwitterKoreanProcessorJava.tokensToJavaKoreanTokenList(stemmed));
+    System.out.println(OpenKoreanTextProcessorJava.tokensToJavaKoreanTokenList(stemmed));
     // [한국어(Noun: 0, 3), 를(Josa: 3, 1),  (Space: 4, 1), 처리(Noun: 5, 2), 하다(Verb: 7, 2),  (Space: 9, 1), 예시(Noun: 10, 2), 이다(Adjective: 12, 3), ㅋㅋ(KoreanParticle: 15, 2),  (Space: 17, 1), #한국어(Hashtag: 18, 4)]
 
 
     // Phrase extraction
-    List<KoreanPhraseExtractor.KoreanPhrase> phrases = TwitterKoreanProcessorJava.extractPhrases(tokens, true, true);
+    List<KoreanPhraseExtractor.KoreanPhrase> phrases = OpenKoreanTextProcessorJava.extractPhrases(tokens, true, true);
     System.out.println(phrases);
     // [한국어(Noun: 0, 3), 처리(Noun: 5, 2), 처리하는 예시(Noun: 5, 7), 예시(Noun: 10, 2), #한국어(Hashtag: 18, 4)]
 
